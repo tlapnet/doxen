4911b3b518bf8151c4a030ea1c5886102f3cb5f2
Josef Benjač <josefbenjac@gmail.com>
2017-12-14 16:32:40 +0100

    Feature: Git decorator

--- src/Bridge/Git/GitDecorator.php
+++ src/Bridge/Git/GitDecorator.php
@@ -2,12 +2,13 @@
 
 namespace Tlapnet\Doxen\Bridge\Git;
 
+use DateTime;
 use Nette\Bridges\ApplicationLatte\Template;
 use Tlapnet\Doxen\Component\IListener;
 use Tlapnet\Doxen\Event\AbstractEvent;
 use Tlapnet\Doxen\Event\NodeEvent;
 use Tlapnet\Doxen\Tree\AbstractNode;
-use Tlapnet\Doxen\Tree\TextNode;
+use Tlapnet\Doxen\Tree\FileNode;
 use Tlapnet\Doxen\Widget\WidgetManager;
 use Tlapnet\Doxen\Widget\Widgets;
 
@@ -31,20 +32,39 @@ class GitDecorator implements IListener
 	 */
 	private function decorateNode(NodeEvent $event)
 	{
-		/** @var TextNode $node */
+		/** @var FileNode $node */
 		$node = $event->getNode();
 
 		if ($node->getType() !== AbstractNode::TYPE_LEAF) {
 			return;
 		}
 
-		if (!($node instanceof TextNode)) {
+		if (!($node instanceof FileNode)) {
+			return;
+		}
+
+		if (!file_exists($node->getFilename())) {
 			return;
 		}
 
 		$wm = new WidgetManager($node);
-		$wm->get(Widgets::PAGE_MENU)->add('git', function (Template $template) {
+		$wm->get(Widgets::PAGE_MENU)->add('git', function (Template $template) use ($node) {
+			$fileName = $node->getFilename();
+			$dirName = dirname($node->getFilename());
+
+			$author = exec('git -C ' . $dirName . ' log -1 --format="%cn" -- ' . $fileName);
+			$email = exec('git -C ' . $dirName . ' log -1 --format="%ce" -- ' . $fileName);
+			$date = exec('git -C ' . $dirName . ' log -1 --format="%cd" -- ' . $fileName);
+
+			if (!$author && !$date)
+				return NULL;
+
+			$template->author = $author;
+			$template->email = $email;
+			$template->date = new DateTime($date);
 			$template->setFile(__DIR__ . '/templates/git.latte');
+
+			return $template;
 		});
 	}
 
