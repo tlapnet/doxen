c26474dcb746729a112f57e22983c57449200ab2
Josef Benjač <josefbenjac@gmail.com>
2017-12-14 18:15:44 +0100

    Feature: Git Decorator metadata

--- src/Bridge/Git/GitDecorator.php
+++ src/Bridge/Git/GitDecorator.php
@@ -24,23 +24,53 @@ class GitDecorator extends AbstractNodeListener
 
 		$wm = new WidgetManager($node);
 		$wm->get(Widgets::PAGE_MENU)->add('git', function (Template $template) use ($node) {
-			$fileName = $node->getFilename();
-			$dirName = dirname($node->getFilename());
+			$author = self::git($node, 'log -1 --format="%cn"', TRUE);
+			$email = self::git($node, 'log -1 --format="%ce"', TRUE);
+			$date = self::git($node, 'log -1 --format="%cd"', TRUE);
+			$date = new DateTime($date);
 
-			$author = exec('git -C ' . $dirName . ' log -1 --format="%cn" -- ' . $fileName);
-			$email = exec('git -C ' . $dirName . ' log -1 --format="%ce" -- ' . $fileName);
-			$date = exec('git -C ' . $dirName . ' log -1 --format="%cd" -- ' . $fileName);
+			$topLevel = self::git($node, 'rev-parse --show-toplevel');
+			$gitFileName = str_replace($topLevel . '/', NULL, $node->getFilename());
+			$currentBranch = self::git($node, 'rev-parse --abbrev-ref HEAD');
+			$originUrl = self::git($node, 'remote get-url origin');
+
+			$node->setMetadataPart('git', [
+				'lastCommiterName' => $author,
+				'lastCommiterEmail' => $email,
+				'lastCommiterDate' => $date,
+				'topLevel' => $topLevel,
+				'fileName' => $gitFileName,
+				'currentBranch' => $currentBranch,
+				'originUrl' => $originUrl,
+			]);
 
 			if (!$author && !$date)
 				return NULL;
 
 			$template->author = $author;
 			$template->email = $email;
-			$template->date = new DateTime($date);
+			$template->date = $date;
 			$template->setFile(__DIR__ . '/templates/git.latte');
 
 			return $template;
 		});
+	}
+
+	/**
+	 * @param FileNode $node
+	 * @param string $command
+	 * @param bool $appendFileName
+	 * @return string
+	 */
+	private static function git(FileNode $node, $command, $appendFileName = FALSE)
+	{
+		$fileName = $node->getFilename();
+		$dirName = dirname($fileName);
+		$command = 'git -C ' . $dirName . ' ' . $command;
+		if ($appendFileName) {
+			$command .= ' -- ' . $fileName;
+		}
+		return exec($command);
 	}
 
 }
